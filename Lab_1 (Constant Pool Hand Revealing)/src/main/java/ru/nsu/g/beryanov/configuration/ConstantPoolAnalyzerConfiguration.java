9c20011d401e11759aca5522f17f7a8ff787d06a
Maxim Beryanov <33306395+beryanow@users.noreply.github.com>
2020-09-26 18:07:08 +0700

    Update ConstantPoolAnalyzerConfiguration.java

--- Lab_1 (Constant Pool Hand Revealing)/src/main/java/ru/nsu/g/beryanov/configuration/ConstantPoolAnalyzerConfiguration.java
+++ Lab_1 (Constant Pool Hand Revealing)/src/main/java/ru/nsu/g/beryanov/configuration/ConstantPoolAnalyzerConfiguration.java
@@ -1,6 +1,7 @@
 package ru.nsu.g.beryanov.configuration;
 
 import lombok.SneakyThrows;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
