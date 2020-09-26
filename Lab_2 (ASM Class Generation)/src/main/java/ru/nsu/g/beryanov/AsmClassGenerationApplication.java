152d2c03ab27610221af9c100c613327c7d197db
Maxim Beryanov <33306395+beryanow@users.noreply.github.com>
2020-09-26 18:04:30 +0700

    Update AsmClassGenerationApplication.java

--- Lab_2 (ASM Class Generation)/src/main/java/ru/nsu/g/beryanov/AsmClassGenerationApplication.java
+++ Lab_2 (ASM Class Generation)/src/main/java/ru/nsu/g/beryanov/AsmClassGenerationApplication.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import ru.nsu.g.beryanov.service.GuessNumberService;
 
 @SpringBootApplication
