b9cab879d6ec3c27fbc228c49c7faa5ccdb9b708
Maxim Beryanov <berjnov.ru@mail.ru>
2020-12-19 15:34:17 +0700

    final project...

--- Final Project (Restful Book Library)/book-library/book-library-service/src/main/java/ru/nsu/g/beryanov/book_library/repository/AddressRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.nsu.g.beryanov.book_library.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.nsu.g.beryanov.book_library.model.Address;
-
-@Repository
-public interface AddressRepository extends JpaRepository<Address, Long> {}
